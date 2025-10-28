a5df07512e5879451e673f255653816677c5c3b8
kirzhdd <kirzhdd@gmail.com>
2025-10-28 18:31:49 +0300

    changed

--- src/modules/numbers/natural/natural.cpp
+++ src/modules/numbers/natural/natural.cpp
@@ -40,21 +40,20 @@ std::string Natural::as_string() const {
 int Natural::COM_NN_D(const Natural& other) const {
     if (n_ > other.n_) {
         return 2; 
-    } else if (n_ < other.n_) {
+    } else if (n_ < other.n_){
         return 1; 
-    } else {
-        //  если количество разрядов равно то сравниваем
-        //  поразрядно, начиная со старшего
-        //  если ни разу не сработал return - числа равны
-        for (int i = n_; i >= 0; --i) {
-            if (a_[i] > other.a_[i]) {
-                return 2; 
-            } else if (a_[i] < other.a_[i]) {
-                return 1; 
-            }
-        }
-        return 0;
     }
+    //  если количество разрядов равно то сравниваем
+    //  поразрядно, начиная со старшего
+    //  если ни разу не сработал return - числа равны
+    for (int i = n_; i >= 0; --i) {
+        if ((a_[i] - '0') > (other.a_[i] - '0')) {
+            return 2; 
+        } else if ((a_[i] - '0') < (other.a_[i] - '0')) {
+            return 1; 
+        }
+    }
+    return 0;
 }
 
 //N-3
@@ -63,49 +62,31 @@ Natural Natural::ADD_1N_N(){
 }
 
 //N-4
-//(!) тут нужен модуль COM_NN_D
 Natural Natural::ADD_NN_N(const Natural& other) const {
-    Natural smaller, larger;
-    if(COM_NN_D(other) == 1){  
-        smaller = *this;
-        larger = other;
-    } else {  
-        smaller = other;
-        larger = *this;
-    }
+    Natural smaller = *this; Natural larger = other;
+    if(COM_NN_D(other) != 1) std::swap(smaller, larger);
 
     int transfer = 0;
 
-    //максимальная длина суммы - длина наибольшего + 1, заполняем нулями
-    std::string result_sum;
-    for(int j = 0; j < larger.n_ + 1; j++){
-        result_sum.push_back('0');
-    }
+    std::string result = larger.as_string();
 
-    int i = 0;
-    for(; i < smaller.n_; i++){  
-        int digit = (larger.a_[i] - '0') + (smaller.a_[i] - '0') + transfer;
+    for(int i = 0; i < larger.n_; i++){  
+        int dig_smaller = (i < smaller.n_) ? (smaller.a_[i] - '0') : 0;
+        int dig_larger = larger.a_[i] - '0';
+        int digit = dig_larger + dig_smaller + transfer;
         transfer = digit / 10;
-        result_sum[i] = (digit % 10) + '0';
-    }
-
-    for(; i < larger.n_; i++){ 
-        int digit = (larger.a_[i] - '0') + transfer;
-        transfer = digit / 10;
-        result_sum[i] = (digit % 10) + '0';
-
+        result[larger.n_ - i - 1] = (digit % 10) + '0';
     }
 
     if(transfer > 0){
-        result_sum[i] = transfer + '0';
+        char ch_dig = transfer + '0';
+        result = ch_dig + result;
     }
 
-    reverse(result_sum.begin(), result_sum.end());
-
-    return Natural(result_sum);
+    return Natural(result);
 }
 
-//N-5
+
 Natural Natural::SUB_NN_N(const Natural& other) const{
     if(COM_NN_D(other) == 1){
         throw std::invalid_argument("Вычитаемое больше уменьшаемого");
@@ -115,47 +96,35 @@ Natural Natural::SUB_NN_N(const Natural& other) const{
         return Natural();
     }
 
-    std::string result;
-    int borrow = 0; //изначально "заем" равен нулю
-    int i = 0;
-
-    for(; i < other.n_; i++){
-        int digit1 = a_[i] - '0' - borrow;
-        int digit2 = other.a_[i] - '0';
-
-        if(digit1 < digit2){
-            digit1 += 10;
-            borrow = 1;
-        } else {
-            borrow = 0;
-        }
-
-        char dig = (digit1 - digit2) + '0';
-        result = dig + result;
-    }
-
-    for (; i < n_; ++i) {
-        int digit = a_[i] - '0' - borrow;
-            
-        if (digit < 0) {
-            digit += 10;
+    std::string result = as_string();
+    int borrow = 0;
+    
+    for(int i = 0; i < n_; i++){
+        int dig1 =  (a_[i] - '0');
+        int dig2 = (i < other.n_) ? (other.a_[i] - '0') : 0;
+        
+        dig1 -= borrow; 
+        
+        if(dig1 < dig2){
+            dig1 += 10;
             borrow = 1;
         } else {
             borrow = 0;
         }
         
-        char dig = (digit + '0');
-        result = dig + result;
+        int diff = dig1 - dig2;
+        result[n_ - i - 1] =  (diff + '0');
     }
-
+    
     return Natural(result);
 }
+
 
 
 //N-6
 Natural Natural::MUL_ND_N(int digit) const{
     if(digit > 10 || digit < 0) {
-        throw std::invalid_argument("Множитель должен быть цифрой");
+        throw std::invalid_argument("Множитель не может быть отрицательным и >= 9");
     }
 
     if(digit == 0 || (n_ == 0 && a_[0] == '0')) return Natural();
@@ -169,13 +138,15 @@ Natural Natural::MUL_ND_N(int digit) const{
         int num = (a_[i] - '0') * digit + transfer;
         transfer = num / 10;
         char dig = (num % 10) + '0';
-        result = dig + result ;
+        result += dig;
     }
 
     if(transfer > 0){
         char dig = transfer + '0';
-        result = dig + result;
+        result += dig;
     }
+
+    reverse(result.begin(), result.end());
 
     return Natural(result);
 }
@@ -185,15 +156,13 @@ Natural Natural::MUL_ND_N(int digit) const{
 Natural Natural::MUL_Nk_N(int k) const{
     if(n_ == 0 && a_[0] == '0') return Natural();
 
-    Natural result = *this;
+    std::string result = as_string();
 
     for(int i = 0; i < k; i++){
-        result.a_ = '0' + result.a_;
+        result += '0';
     }
 
-    result.n_ += k;
-
-    return result;
+    return Natural(result);
 }
 
 
@@ -207,10 +176,6 @@ Natural Natural::SUB_NDN_N(const Natural& other, int digit) const{
 
 
 //N-10
-//(!) в указанных зависимостях не было указано MUL_ND_N
-//    но без него никак, только если костылями, путем перебора
-//    всех цифр -> создания из каждой цифры экземпляра Natural и умножения
-//    его на 10^k
 std::pair<int, int> Natural::DIV_NN_Dk(const Natural& other) const{
     int k = 0; 
     while(COM_NN_D(other.MUL_Nk_N(k)) != 1){
