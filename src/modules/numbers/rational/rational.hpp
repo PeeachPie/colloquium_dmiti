a6509f7d978cad81f75d7482e6c3285577b42878
Prok-Al <prokopjevaleks@gmail.com>
2025-10-30 00:05:25 +0300

    refactor(rational.h): [[nodiscard]] marks added

--- src/modules/numbers/rational/rational.hpp
+++ src/modules/numbers/rational/rational.hpp
@@ -18,23 +18,23 @@ public:
     Rational& RED_Q_Q();
     
     // Q-2 | Проверка сокращенного дробного на целое, если рациональное число является целым, то «да», иначе «нет»
-    bool INT_Q_B() const;
+    [[nodiscard]] bool INT_Q_B() const;
     
     // Q-3 | Преобразование целого в дробное
-    Rational TRANS_Z_Q(const Integer& integer);
+    [[nodiscard]] static Rational TRANS_Z_Q(const Integer& integer);
     
     // Q-4 | Преобразование сокращенного дробного в целое (если знаменатель равен 1)
-    Integer TRANS_Q_Z() const;
+    [[nodiscard]] Integer TRANS_Q_Z() const;
     
     // Q-5 | Сложение дробей
-    Rational ADD_QQ_Q(const Rational& other) const;
+    [[nodiscard]] Rational ADD_QQ_Q(const Rational& other) const;
     
     // Q-6 | Вычитание дробей
-    Rational SUB_QQ_Q(const Rational& other) const;
+    [[nodiscard]] Rational SUB_QQ_Q(const Rational& other) const;
     
     // Q-7 | Умножение дробей
-    Rational MUL_QQ_Q(const Rational& other) const;
+    [[nodiscard]] Rational MUL_QQ_Q(const Rational& other) const;
     
     // Q-8 | Деление дробей (делитель отличен от нуля)
-    Rational DIV_QQ_Q(const Rational& other) const;
+    [[nodiscard]] Rational DIV_QQ_Q(const Rational& other) const;
 };
